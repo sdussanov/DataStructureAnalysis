2be1ded0444145143ae274c8204961330ccc8b06
sdussanov <safarkhan.dussanov@gmail.com>
2023-08-18 00:39:01 +0600

    init

--- src/StringBalanced.java
+++ src/StringBalanced.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class StringBalanced {
+    private final List<Character> leftBrackets = Arrays.asList('{','<','[','(');
+    private final List<Character> rightBrackets = Arrays.asList('}','>',']',')');
 
     public boolean isBalanced(String input) {
         Stack<Character> stack = new Stack<>();
@@ -11,42 +14,25 @@ public class StringBalanced {
                 stack.push(ch);
 
             if (isRightBracket(ch)) {
-                if (stack.empty())
-                    return false;
+                if (stack.empty()) return false;
 
                 var left = stack.pop();
                 if (!isBracketsMatch(left, ch)) return false;
             }
         }
 
-
         return stack.isEmpty();
     }
 
     private boolean isLeftBracket(char ch) {
-        return ch == '{' || ch == '<' || ch == '[' || ch == '(';
+        return leftBrackets.contains(ch);
     }
 
     private boolean isRightBracket(char ch) {
-        return ch == '}' || ch == '>' || ch == ']' || ch == ')';
+        return rightBrackets.contains(ch);
     }
 
     private boolean isBracketsMatch(char left, char right) {
-        switch (left) {
-            case '}':
-                if (right != '{')
-                    return false;
-            case '>':
-                if (right != '<')
-                    return false;
-            case ']':
-                if (right != '[')
-                    return false;
-            case ')':
-                if (right != '(')
-                    return false;
-            default:
-                return true;
-        }
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
