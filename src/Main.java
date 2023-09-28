2c932f1fafaff50623b34db9b5a8e1c17a38974e
sdussanov <safarkhan.dussanov@gmail.com>
2023-09-28 22:27:43 +0600

    added new solutions

--- src/Main.java
+++ src/Main.java
@@ -142,10 +142,11 @@ public class Main {
         /* 0013-Tree */
 
         Tree tree = new Tree();
-        tree.insert(7);
+        tree.insert(1);
         tree.insert(5);
         tree.insert(4);
         tree.insert(10);
+        System.out.println(tree.find());
         System.out.println(tree);
     }
 }
