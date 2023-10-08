0f5acb81fdebb9a2b33bb9f6825975a605dbc012
sdussanov <safarkhan.dussanov@gmail.com>
2023-10-08 20:50:25 +0600

    added new solutions

--- src/Main.java
+++ src/Main.java
@@ -141,16 +141,15 @@ public class Main {
 
         /* 0013-Tree */
 
-        Tree tree = new Tree();
-        tree.insert(7);
-        tree.insert(4);
-        tree.insert(9);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
-        tree.insert(10);
-        System.out.println(tree.find(1));
-
+//        Tree tree = new Tree();
+//        tree.insert(7);
+//        tree.insert(4);
+//        tree.insert(9);
+//        tree.insert(1);
+//        tree.insert(6);
+//        tree.insert(8);
+//        tree.insert(10);
+//        System.out.println(tree.find(1));
 
     }
 }
